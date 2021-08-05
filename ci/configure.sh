9d6cc6d3d9f37a15c6a0e7cf75a771acdb0f29e1
Joseph Palermo <jpalermo@pivotal.io>
2021-08-04 22:08:37 -0700

    Move to bosh-ecosystem concourse

--- ci/configure.sh
+++ ci/configure.sh
@@ -4,9 +4,7 @@ set -eux
 
 dir=$(dirname $0)
 
-fly -t production login -n bosh-packages -u bosh-packages
-
-fly -t production set-pipeline \
+fly -t bosh-ecosystem set-pipeline \
 	-p nginx-release \
 	-c $dir/pipeline.yml \
 	-l <(lpass show --note "bosh-packages-concourse-vars-nginx-release")
